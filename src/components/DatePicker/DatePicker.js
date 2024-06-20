13c3e7f2dabc23579eb4efa5245b7f3927eea5e0
akshat170902 <akshat.ug20@nsut.ac.in>
2024-06-20 22:20:27 +0530

    improved design of date picker

--- src/components/DatePicker/DatePicker.js
+++ src/components/DatePicker/DatePicker.js
@@ -2,16 +2,17 @@ import React from 'react';
 import { DatePicker } from 'antd';
 import dayjs from 'dayjs';
 import customParseFormat from 'dayjs/plugin/customParseFormat';
+import { SlArrowDown } from "react-icons/sl";
 dayjs.extend(customParseFormat);
 const dateFormat = 'YYYY-MM-DD';
 const App = ({inputValues,setInputValues}) => (
-  
   <DatePicker
     defaultValue={dayjs()}
     minDate={dayjs()}
     maxDate={dayjs().add(12,'month')}
-    style={{border:"2px solid"}}
+    style={{border:"grey 2px solid" ,width:"200px",height:"60px",color:"black"}}
     allowClear={false}
+    suffixIcon={<SlArrowDown />}
     value={inputValues.startDate===''?null:dayjs(inputValues.startDate)}
             onChange={(newValue) => {
               const currentDate = dayjs(newValue);
