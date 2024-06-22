db32cbe754dc5d665c6afafed513763e6eaff3d1
tmoudgil <tusharmoudgil22@gmail.com>
2024-06-22 18:12:43 +0530

    added user login check before chat now button

--- src/components/Trip/Trip.js
+++ src/components/Trip/Trip.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, {useContext} from 'react';
+import {UserLoginContext} from "../../Utils/Context/UserLoginContext";
 import "./Trip.css";
+import { Navigate, useNavigate } from 'react-router-dom';
 
 const Trip = ({trip})=>{
+
+  const {userLoginData, setUserLoginData} = useContext(UserLoginContext);
+  const navigate = useNavigate();
+
+  const handleClickOnChatButton = ()=>{
+    if(userLoginData.isUserLoggedIn){
+      
+    }
+    else{
+      navigate("/login-page");
+    }
+  }
+
    
   return (
     <div className="cards">
@@ -29,7 +44,7 @@ const Trip = ({trip})=>{
           <p><strong>Age:</strong>{trip.age}</p>
           <p><strong>Sex:</strong> {trip.sex}</p>
           <p className='description'><strong>Description:</strong>{trip.description} </p>
-          <button className="chat-now-btn" role="button">Chat Now</button>
+          <button className="chat-now-btn" role="button" onClick={handleClickOnChatButton}>Chat Now</button>
           </div>
       </div>
     </a>      
