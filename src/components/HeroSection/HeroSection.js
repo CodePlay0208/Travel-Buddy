042c163cd29fe7750e98775127a1f8ec263f3e50
tmoudgil <tusharmoudgil22@gmail.com>
2024-06-24 22:40:05 +0530

    modified the home page

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -15,7 +15,7 @@ const Hero = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3000, // 3000 milliseconds = 3 seconds
+    autoplaySpeed: 2000, // 3000 milliseconds = 3 seconds
     // nextArrow: <NextArrow />,
     // prevArrow: <PrevArrow />,
   };
@@ -42,8 +42,8 @@ const Hero = () => {
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-11h2v2h-2v-2zm0 4h2v6h-2v-6z"/></svg>
             </div>
             <div className="description-container">
-              <h2 className="content-title">Your pick of rides at low prices</h2>
-              <p className="content-description">No matter where you’re going, by bus or carpool, find the perfect ride from our wide range of destinations and routes at low prices.</p>
+              <h2 className="content-title">Finding the best group</h2>
+              <p className="content-description">No matter where you’re going, find the perfect trip and the perfect group from our wide range of destinations and routes.</p>
             </div>
           </li>
           <li className="content-item">
@@ -52,7 +52,7 @@ const Hero = () => {
             </div>
             <div className="description-container">
               <h2 className="content-title">Trust who you travel with</h2>
-              <p className="content-description">We take the time to get to know each of our members and bus partners. We check reviews, profiles and IDs, so you know who you’re travelling with and can book your ride at ease on our secure platform.</p>
+              <p className="content-description">We take the time to get to know each of our trip publishers and members. We check reviews, profiles and IDs, so you know who you’re travelling with and can find a group at ease on our secure platform.</p>
             </div>
           </li>
           <li className="content-item">
@@ -61,7 +61,7 @@ const Hero = () => {
             </div>
             <div className="description-container">
               <h2 className="content-title">Scroll, click, tap and go!</h2>
-              <p className="content-description">Booking a ride has never been easier! Thanks to our simple app powered by great technology, you can book a ride close to you in just minutes.</p>
+              <p className="content-description">Finding a trip has never been easier! Thanks to our simple app powered by great technology, you can book a trip close to you in just minutes.</p>
             </div>
           </li>
         </ul>
