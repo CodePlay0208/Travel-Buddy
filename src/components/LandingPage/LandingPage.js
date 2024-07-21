fdf2a2f6b24c19a4ef827f0b0280905bdecba88d
akshat170902 <akshat.ug20@nsut.ac.in>
2024-07-22 00:39:21 +0530

    herosection:

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -3,12 +3,14 @@ import './LandingPage.css';
 import Header from './Header/Header';
 import PopularSection from './PopularSection/PopularSection';
 import AboutSection from './AboutSection/AboutSection';
+import HeroSection from './HeroSection/HeroSection';
 const LandingPage = () => {
   return (
     <div className="LandingContainer">
       <Header/>
       <PopularSection/>
         <AboutSection/>
+        <HeroSection/>
     </div>
   );
 }
