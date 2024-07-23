bf2ccc2afe4b7c17e6f99d4313cbea382033012f
akshat170902 <akshat.ug20@nsut.ac.in>
2024-07-23 12:06:46 +0530

    top Destination section

--- src/components/LandingPage/TopDestination/TopDestination.js
+++ src/components/LandingPage/TopDestination/TopDestination.js
@@ -12,12 +12,12 @@ const TopDestination = () => (
     />
     <div className="rectangle40" />
     <div className="heading">Where do you want to go?</div>
-    <button className="explore-button">Explore more places</button>
     <div className="location-container">
       <button className="location-box">Bangalore</button>
       <button className="location-box">Tamil Nadu</button>
       <button className="location-box">Mumbai</button>
     </div>
+    <button className="explore-button">Explore more places</button>
   </div>
 );
 
