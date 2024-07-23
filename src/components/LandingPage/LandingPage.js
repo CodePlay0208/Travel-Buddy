200685ac3a8c72e009663e016aaef89fe79da6a9
akshat170902 <akshat.ug20@nsut.ac.in>
2024-07-23 11:54:44 +0530

    Top Destinations

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -4,6 +4,7 @@ import Header from './Header/Header';
 import PopularSection from './PopularSection/PopularSection';
 import AboutSection from './AboutSection/AboutSection';
 import HeroSection from './HeroSection/HeroSection';
+import TopDestination from './TopDestination/TopDestination';
 const LandingPage = () => {
   return (
     <div className="LandingContainer">
@@ -11,6 +12,7 @@ const LandingPage = () => {
       <PopularSection/>
         <AboutSection/>
         <HeroSection/>
+        <TopDestination/>
     </div>
   );
 }
---
+++ src/components/LandingPage/TopDestination/TopDestination.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './TopDestination.css';
+
+import firstImage from '../../../data/image.png';
+
+const TopDestination = () => (
+  <div className="frame">
+    <img 
+      src={firstImage} 
+      alt="" 
+      className="background-image" 
+    />
+    <div className="rectangle40" />
+    <div className="heading">Where do you want to go?</div>
+    <button className="explore-button">Explore more places</button>
+    <div className="location-container">
+      <button className="location-box">Bangalore</button>
+      <button className="location-box">Tamil Nadu</button>
+      <button className="location-box">Mumbai</button>
+    </div>
+  </div>
+);
+
+export default TopDestination;
