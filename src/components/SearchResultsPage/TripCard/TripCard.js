8a4f979a026916dbf15c53b6ac82ebfe72a6e5f1
akshat170902 <akshat.ug20@nsut.ac.in>
2024-07-27 21:30:14 +0530

    added

--- src/components/SearchResultsPage/TripCard/TripCard.js
+++ src/components/SearchResultsPage/TripCard/TripCard.js
@@ -1,19 +1,60 @@
 import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import './TripCard.css'; // Import your styles
 
-const TripCard = ({ name, profileImg, startLocation, endLocation, totalMembers, age, gender, description, destinationImages }) => {
+const TripCard = ({ name, profileImg, startDate, endDate, startLocation, endLocation, totalMembers, age, gender, description, destinationImages }) => {
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
+
+  const truncateDescription = (text, maxLength) => {
+    if (text.length <= maxLength) {
+      return text;
+    }
+    return text.slice(0, maxLength) + '...';
+  };
+
   return (
-    <div className="trip-card">
-      <img src={profileImg} alt={`${name}'s profile`} className="profile-img"/>
-      <h3>{name}</h3>
-      <p>{startLocation} to {endLocation}</p>
-      <p>Members: {totalMembers}</p>
-      <p>Age: {age}, Gender: {gender}</p>
-      <p>{description}</p>
-      <div className="destination-images">
-        {destinationImages.map((img, index) => (
-          <img key={index} src={img} alt={`Destination ${index + 1}`} className="destination-img"/>
-        ))}
+    <div className="tripCard__container">
+      <div className="tripCard__leftContainer">
+        <Slider {...settings}>
+          {destinationImages.map((img, index) => (
+            <div key={index} className="tripCard__carouselItem">
+              <img src={img} alt={`Destination ${index + 1}`} className="tripCard__destinationImg" />
+            </div>
+          ))}
+        </Slider>
+      </div>
+      <div className="tripCard__rightContainer">
+        <div className="tripCard__profileSection">
+          <img src={profileImg} alt={`${name}'s profile`} className="tripCard__profileImg" />
+          <h3 className="tripCard__username">{name}</h3>
+        </div>
+        <div className="tripCard__details">
+          <div className="tripCard__separatorLine"></div>
+          <div className="tripCard__locations">
+            <div className="tripCard__detailsLeft">
+              <div className="tripCard__dateLabel">Start date: {startDate}</div>
+              <div className="tripCard__separatorLine"></div>
+              <div className="tripCard__dateLabel">End date: {endDate}</div>
+            </div>
+            <div className="tripCard__detailsRight">
+              <div className="tripCard__dateLabel">Start Location: {startLocation}</div>
+              <div className="tripCard__separatorLine"></div>
+              <div className="tripCard__dateLabel">End Location: {endLocation}</div>
+              <div className="tripCard__separatorLine"></div>
+            </div>
+          </div>
+          <div className="tripCard__description">Desc. {truncateDescription(description, 50)}</div>
+        </div>
+        <div className="tripCard__chatNow">
+          <button className="tripCard__chatButton">Chat Now</button>
+        </div>
       </div>
     </div>
   );
