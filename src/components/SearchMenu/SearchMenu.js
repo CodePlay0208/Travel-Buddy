dbe30b4fc299d11bb9b426a2364fa55d77c2e88d
akshat170902 <akshat.ug20@nsut.ac.in>
2024-08-11 20:25:05 +0530

    revised the search result page

--- src/components/SearchMenu/SearchMenu.js
+++ src/components/SearchMenu/SearchMenu.js
@@ -1,6 +1,6 @@
 import React, {useContext, useEffect , memo} from 'react';
 import './SearchMenu.css';
-import SearchBar from '../SearchBar/SearchBar';
+import SearchBar from '../SearchBar/Searchbar';
 import DatePicker from '../DatePicker/DatePicker';
 import { InputValuesContext } from '../../Utils/Context/InputValuesContext';
 
--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './SearchBar.css';
+import './Searchbar.css';
 import { SVG } from '../../assets'
 
 
