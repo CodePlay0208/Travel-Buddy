01abdc0b1cdbe371bb9a52ff870a6b4c6a9b38ad
Chirayu Jhakal <jhakalchirayu@gmail.com>
2024-08-17 18:50:25 +0530

    minor error

--- src/components/PublishTrip/PublishTrip.js
+++ src/components/PublishTrip/PublishTrip.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useContext } from "react";
 import Navbar from '../NavBar/NavBar';
 import Footer from '../Footer/Footer';
 import "./PublishTrip.css";
-import SearchBar from "../SearchBar/Searchbar";
+import SearchBar from "../SearchBar/SearchBar";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useNavigate } from "react-router-dom";
