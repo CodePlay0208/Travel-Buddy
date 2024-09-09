4fb93cb20d76a05ac06a45a9cb66522c747fd53a
akshat170902 <akshat.ug20@nsut.ac.in>
2024-09-09 20:04:45 +0530

    added the basic struct

---
+++ src/components/styles/Global.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+const GlobalStyles= createGlobalStyle`
+*{
+    box-sizing:border-box;
+}
+`
+
+export default GlobalStyles;
