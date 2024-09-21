ab08b0cce9c3835451f0b541181c14687b4a5284
akshat170902 <akshat.ug20@nsut.ac.in>
2024-09-21 15:38:41 +0530

    common commit

--- src/components/TopDestination/TopDestination.js
+++ src/components/TopDestination/TopDestination.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import './TopDestination.css';
+import { 
+  Frame, 
+  BackgroundImage, 
+  Rectangle40, 
+  Heading, 
+  LocationContainer, 
+  LocationBox, 
+  ExploreButton 
+} from '../styles/TopDestination.styled.js';
 
 import firstImage from '../../data/image.png';
 
 const TopDestination = () => (
-  <div className="frame">
-    <img 
-      src={firstImage} 
-      alt="" 
-      className="background-image" 
-    />
-    <div className="rectangle40" />
-    <div className="heading">Where do you want to go?</div>
-    <div className="location-container">
-      <button className="location-box">Bangalore</button>
-      <button className="location-box">Tamil Nadu</button>
-      <button className="location-box">Mumbai</button>
-    </div>
-    <button className="explore-button">Explore more places</button>
-  </div>
+  <Frame>
+    <BackgroundImage src={firstImage} alt="" />
+    <Rectangle40 />
+    <Heading>Where do you want to go?</Heading>
+    <LocationContainer>
+      <LocationBox>Bangalore</LocationBox>
+      <LocationBox>Tamil Nadu</LocationBox>
+      <LocationBox>Mumbai</LocationBox>
+    </LocationContainer>
+    <ExploreButton>Explore more places</ExploreButton>
+  </Frame>
 );
 
 export default TopDestination;
