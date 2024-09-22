6322feade882e48b186de636de502de8fa5ec705
akshat170902 <akshat.ug20@nsut.ac.in>
2024-09-22 22:59:40 +0530

    search result page pending

---
+++ src/Screens/SearchResultsPage/SearchResultsPage.styled.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+`
+
+export const TripList = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  margin-top: 5%;
+  width: 100%;
+`
+
+export const ButtonDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 1%;
+`
+
+export const ShowMoreButton = styled.button`
+  font-size: 36px;
+  padding: 10px 20px;
+  cursor: pointer;
+  background-color: #8dd3bb;
+  width: 268px;
+  border-radius: 10px;
+  font-weight: 700;
+  border: none;
+  color: rgb(0, 0, 0);
+  padding: 2%;
+`
