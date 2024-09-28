e7550301146ecdb23911ffc07556ab8f8f9d5039
akshat170902 <akshat.ug20@nsut.ac.in>
2024-09-28 22:18:31 +0530

    verify code

--- src/Screens/AuthFlow/VerifyCode/VerifyCode.js
+++ src/Screens/AuthFlow/VerifyCode/VerifyCode.js
@@ -2,9 +2,13 @@ import React, { useEffect, useState, memo } from 'react'
 import { useNavigate, useLocation } from 'react-router-dom'
 import { SVG } from '../../../assets'
 import './VerifyCode.css'
+
+import '../AuthFlow.css'
 import { connect } from 'react-redux'
 import { verifyOTP, resendOTP } from '../../../actions/auth.action'
 import { ToastContainer } from 'react-toastify'
+import InputComponent from '../InputComponent/InputComponent'
+import Copyright from '../../../components/Copyright/Copyright'
 
 const mapStateToProps = (state) => ({
   otpVerified: state.auth.otpVerified,
@@ -15,88 +19,57 @@ const VerifyCode = ({ otpVerified, verifyOTP, resendOTP }) => {
   const location = useLocation()
   const origin = sessionStorage.getItem('prevRoute')
 
-  const [verificationCode, setVerificationCode] = useState('')
+  const [formData, setFormData] = useState({ verificationCode: '' })
   const [secureVerificationCode, setSecureVerificationCode] = useState(true)
-
-  const handleVerificationCodeChange = (e) => {
-    setVerificationCode(e.target.value)
-  }
 
   const handleBackButtonClick = () => {
     navigate(-1)
   }
 
   const onSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefault()
     const isSignUpRequest = origin === '/signup'
-    verifyOTP(verificationCode, isSignUpRequest)
+    verifyOTP(formData.verificationCode, isSignUpRequest)
   }
 
   const onResendClick = async () => {
     await resendOTP()
   }
 
-  const PasswordEyeComponent = memo((props) => {
-    const { secureTextState, setSecureTextState } = props
-
-    const handlePasswordEyeIconClick = () => {
-      setSecureTextState((prevState) => !prevState)
-    }
-
-    return (
-      <div className="VerifyCodePasswordEyeContainer" role="button" onClick={handlePasswordEyeIconClick}>
-        {secureTextState ? (
-          <img src={SVG.EyeIcon} className="VerifyCodePasswordEyeIcon" />
-        ) : (
-          <img src={SVG.EyeSlashIcon} className="VerifyCodePasswordEyeIcon" />
-        )}
-      </div>
-    )
-  })
-
   useEffect(() => {
     if (otpVerified) {
       if (origin === '/signup') {
-        navigate('/');
+        navigate('/')
       } else if (origin === '/forget-password') {
-        navigate('/set-password');
+        navigate('/set-password')
       }
     }
-  }, [otpVerified, navigate]);
+  }, [otpVerified, navigate])
 
   return (
-    <div className="VerifyCodeContainer">
-      <div className="VerifyCodeFormAndCopyrightContainer">
-        <div className="VerifyCodeFormAndTitleContainer">
-          <div className="VerifyCodeTitleContainer">
-            <p className="VerifyCodeTitleText">Travmigoz</p>
-          </div>
-          <div className="VerifyCodeFormContainer">
-            <div className="VerifyCodeBackButtonContainer" role="button" onClick={handleBackButtonClick}>
-              <img src={SVG.BackButtonIcon} className="VerifyCodeBackButtonIcon" />
-              <p className="VerifyCodeBackButtonText">Back</p>
+    <div className="Container">
+      <div className="FormAndCopyrightContainer">
+        <div className="FormAndTitleContainer">
+          <div className="TitleContainer">Travmigoz</div>
+          <div className="FormContainer">
+            <div className="BackButtonContainer" role="button" onClick={handleBackButtonClick}>
+              <img src={SVG.BackButtonIcon} className="BackButtonIcon" alt="Back" />
+              <p className="BackButtonText">Back</p>
             </div>
-            <div className="VerifyCodeFormHeadingContainer">
-              <p className="VerifyCodeFormHeadingText">Verify code</p>
-            </div>
-            <div className="VerifyCodeFormSubHeadingContainer">
-              <p className="VerifyCodeFormSubHeadingText">An authentication code has been sent to your email.</p>
-            </div>
+            <div className="FormHeadingContainer">Verify code</div>
+            <div className="FormSubHeadingText">An authentication code has been sent to your email.</div>
             <div className="VerifyCodeFormInputsContainer">
               <form onSubmit={onSubmit}>
-                <div className="VerifyCodeCodeInputContainer">
-                  <label className="VerifyCodeCodeText">Enter Code</label>
-                  <input
-                    className="VerifyCodeCodeInput"
-                    type={secureVerificationCode ? 'password' : 'text'}
-                    name="code"
-                    value={verificationCode}
-                    onChange={(e) => handleVerificationCodeChange(e)}
-                    placeholder="Enter The Code"
-                    required
-                  />
-                  <PasswordEyeComponent secureTextState={secureVerificationCode} setSecureTextState={setSecureVerificationCode} />
-                </div>
+                <InputComponent
+                  label="Enter Code"
+                  type={secureVerificationCode ? 'password' : 'text'}
+                  name="verificationCode"
+                  user={formData}
+                  setUser={setFormData}
+                  placeholder="Enter The Code"
+                  secureTextState={secureVerificationCode}
+                  setSecureTextState={setSecureVerificationCode}
+                />
                 <div className="VerifyCodeResendCodeContainer">
                   <p className="VerifyCodeDidntRecieveText">Didn’t receive a code?</p>
                   <a href="#" className="VerifyCodeResendLink" onClick={onResendClick}>
@@ -104,20 +77,18 @@ const VerifyCode = ({ otpVerified, verifyOTP, resendOTP }) => {
                   </a>
                 </div>
                 <div className="VerifyCodeVerifyButtonContainer">
-                  <button type="submit" className="VerifyCodeVerifyButton">Verify</button>
+                  <button type="submit" className="VerifyCodeVerifyButton">
+                    Verify
+                  </button>
                 </div>
               </form>
             </div>
           </div>
         </div>
-        <div className="VerifyCodeCopyrightTextContainer">
-          <p className="VerifyCodeCopyrightText">
-            &copy; 2024 <span className="VerifyCodeTravmigozCopyrightText">Travmigoz</span>. All Rights Reserved
-          </p>
-        </div>
+        <Copyright />
       </div>
-      <div className="VerifyCodeDesignContainer">
-        <img src={SVG.AuthDesignSection} className="VerifyCodeAuthDesignImage" alt="AuthDesignImage" />
+      <div className="DesignContainer">
+        <img src={SVG.AuthDesignSection} className="AuthDesignImage" alt="Auth Design" />
       </div>
       <ToastContainer />
     </div>
