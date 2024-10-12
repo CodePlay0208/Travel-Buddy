65d68673e9600c14241257c9585f80032ca82372
akshat170902 <akshat.ug20@nsut.ac.in>
2024-10-12 23:44:39 +0530

    nav bar responsive

--- src/components/Dropdown/Dropdown.styled.js
+++ src/components/Dropdown/Dropdown.styled.js
@@ -1,5 +1,10 @@
 import styled from 'styled-components'
 
+const breakpoints = {
+  mobile: '480px',
+  tablet: '768px',
+  desktop: '1024px',
+}
 export const DropdownContainer = styled.div`
   position: absolute;
   top: 100%;
@@ -9,6 +14,12 @@ export const DropdownContainer = styled.div`
   border-radius: 20px;
   z-index: 10;
   padding: 2% 0px;
+  @media (max-width: ${breakpoints.tablet}) {
+    border-radius: 15px;
+  }
+  @media (max-width: ${breakpoints.mobile}) {
+    border-radius: 5px;
+  }
 `
 
 export const DropdownItem = styled.div`
@@ -26,5 +37,11 @@ export const DropdownItem = styled.div`
 
   &:hover {
     background-color: #a1a1a1;
+  }
+  @media (max-width: ${breakpoints.tablet}) {
+    border-radius: 5px;
+  }
+  @media (max-width: ${breakpoints.mobile}) {
+    border-radius: 1px;
   }
 `
