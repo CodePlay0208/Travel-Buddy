17d059dd612fba52941ef0c3a60c94921ce99a4e
akshat170902 <akshat.ug20@nsut.ac.in>
2024-10-26 19:03:11 +0530

    Detail box RESPONSIVE

--- src/components/TripPage/DetailsSection/AddMembers/AddMembers.js
+++ src/components/TripPage/DetailsSection/AddMembers/AddMembers.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import DetailBox from '../DetailBox/DetailBox';
+import React from 'react'
+import DetailBox from '../DetailBox/DetailBox'
 import './AddMembers.css'
 const AddMembers = () => {
-    return (
-        
-        <div className="trippage_details-lower-section">
-        <div className="trippage_heading-container">
-            <div className="trippage_head-title">
-                Meet Your Travmigoz
-            </div>
-            <div className="trippage_create-more-button">
-                Create More
-            </div>
-        </div>
+  return (
+    <div className="trippage_details-lower-section">
+      <div className="trippage_heading-container">
+        <div className="trippage_head-title">Meet Your Travmigoz</div>
+        <div className="trippage_create-more-button">Create More</div>
+      </div>
 
-        <div className="trippage_profile-cards">
-            <DetailBox heading={"Username"} body={"Trip Publisher"} svg={""}></DetailBox>
-            <DetailBox heading={"Username"} body={"Member 1"} svg={""}></DetailBox>
-            <DetailBox heading={"Username"} body={"Member 2"} svg={""}></DetailBox>
-        </div>
+      <div className="trippage_profile-cards">
+        <DetailBox heading={'Username'} body={'Trip Publisher'} svg={''}></DetailBox>
+        <DetailBox heading={'Username'} body={'Member 1'} svg={''}></DetailBox>
+        <DetailBox heading={'Username'} body={'Member 2'} svg={''}></DetailBox>
+      </div>
     </div>
-    );
-};
+  )
+}
 
-export default AddMembers;
+export default AddMembers
