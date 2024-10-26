5cbf2617006610d1fe386c5bc5cdc0117c080260
akshat170902 <akshat.ug20@nsut.ac.in>
2024-10-26 21:15:09 +0530

    DetailsSection STYLED and responsive

---
+++ src/components/TripPage/DetailsSection/DetailsSection.styled.js
@@ -0,0 +1,46 @@
+import styled from 'styled-components'
+import backgroundImage from '../../../data/Images/MapImage.png'
+export const SectionContainer = styled.div`
+  position: relative;
+  width: 100%;
+  box-sizing: border-box;
+`
+
+export const Title = styled.div`
+  font-size: 3.5vw;
+  font-weight: 600;
+  line-height: 5vw;
+  margin: 5% 0;
+  text-align: center;
+`
+
+export const UpperSection = styled.div`
+  box-sizing: border-box;
+  margin: 2.5% 0;
+`
+
+export const MapImage = styled.div`
+  background: url(${backgroundImage}) no-repeat center;
+  background-size: cover;
+  width: 100%;
+  height: 60vh;
+
+  @media (max-width: 1080px) {
+    height: 40vh;
+  }
+  @media (max-width: 440px) {
+    height: 30vh;
+  }
+`
+
+export const Divider = styled.div`
+  height: 1px;
+  background-color: grey;
+  width: calc(100% + 8%);
+  margin: 4% -4% 2.5%;
+
+  @media (max-width: 768px) {
+    height: 0.25px;
+    background-color: rgba(128, 128, 128, 0.608);
+  }
+`
---
+++ src/components/TripPage/DetailsSection/TripDescription/TripDescription.js
@@ -0,0 +1,66 @@
+import { useState, React } from 'react'
+import './TripDescription.css'
+const TripDescription = () => {
+  const [isExpanded, setIsExpanded] = useState(false)
+
+  const toggleExpand = () => {
+    setIsExpanded(!isExpanded)
+  }
+
+  const content = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ...`
+
+  const words = content.split(' ')
+  const displayedContent = isExpanded ? content : words.slice(0, 50).join(' ') + '...'
+
+  return (
+    <div className="trippage_des-chat-section">
+      <div className="trippage_des">
+        <div className="trippage_title">Add Title Here</div>
+        <div className="trippage_des-title">Description</div>
+        <div className="grey-line2"></div>
+        <div className="trippage_des-content">
+          {displayedContent}
+          {words.length > 50 && (
+            <span onClick={toggleExpand} className="trippage_toggle-button">
+              {isExpanded ? ' Show Less' : ' Show More'}
+            </span>
+          )}
+        </div>
+      </div>
+      <div className="trippage_chat-section">
+        <div className="trippage_profile-image">
+          <div className="trippage_profile-picture">
+            <img src="" alt="" />
+          </div>
+          <div className="trippage_profileName">User</div>
+        </div>
+        <div className="grey-line2"></div>
+        <div className="trippage_date-container">
+          <div className="trippage_date-section">
+            <div className="trippage_startdate trippage_details">
+              <div className="trippage_box_heading">Start Date</div>
+              <div className="trippage_box_content">DD/MM/YYYY</div>
+            </div>
+            <div className="trippage_enddate trippage_details">
+              <div className="trippage_box_heading">End Date</div>
+              <div className="trippage_box_content">DD/MM/YYYY</div>
+            </div>
+          </div>
+          <div className="trippage_info-section">
+            <div className="trippage_budget trippage_details">
+              <div className="trippage_box_heading">Budget</div>
+              <div className="trippage_box_content">6000</div>
+            </div>
+            <div className="trippage_totalMember trippage_details">
+              <div className="trippage_box_heading">Members</div>
+              <div className="trippage_box_content">10</div>
+            </div>
+          </div>
+          <div className="trippage_chat-now-button">Chat Now</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default TripDescription
