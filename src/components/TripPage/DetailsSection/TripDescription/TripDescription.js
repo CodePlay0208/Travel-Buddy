48a9612840e1287cef4653b3b0326d72a1bf377a
akshat170902 <akshat.ug20@nsut.ac.in>
2024-10-26 23:02:52 +0530

    Tripdescription styled and responsive

--- src/components/TripPage/DetailsSection/TripDescription/TripDescription.js
+++ src/components/TripPage/DetailsSection/TripDescription/TripDescription.js
@@ -1,5 +1,27 @@
-import { useState, React } from 'react'
-import './TripDescription.css'
+import { useState } from 'react'
+import {
+  SectionContainer,
+  DescriptionContainer,
+  Title,
+  DescriptionTitle,
+  DescriptionContent,
+  ToggleButton,
+  ChatSection,
+  ProfileImage,
+  ProfilePicture,
+  ProfileName,
+  DateContainer,
+  DateSection,
+  InfoSection,
+  DetailsBox,
+  BoxHeading,
+  BoxContent,
+  ChatButton,
+  GreyLine,
+  StartDate,
+  EndDate,
+} from './TripDescription.styled'
+
 const TripDescription = () => {
   const [isExpanded, setIsExpanded] = useState(false)
 
@@ -8,58 +30,51 @@ const TripDescription = () => {
   }
 
   const content = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ...`
-
   const words = content.split(' ')
-  const displayedContent = isExpanded ? content : words.slice(0, 50).join(' ') + '...'
+  const displayedContent = isExpanded ? content : words.slice(0, 40).join(' ') + '...'
 
   return (
-    <div className="trippage_des-chat-section">
-      <div className="trippage_des">
-        <div className="trippage_title">Add Title Here</div>
-        <div className="trippage_des-title">Description</div>
-        <div className="grey-line2"></div>
-        <div className="trippage_des-content">
+    <SectionContainer>
+      <DescriptionContainer>
+        <Title>Add Title Here</Title>
+        <DescriptionTitle>Description</DescriptionTitle>
+        <GreyLine />
+        <DescriptionContent>
           {displayedContent}
-          {words.length > 50 && (
-            <span onClick={toggleExpand} className="trippage_toggle-button">
-              {isExpanded ? ' Show Less' : ' Show More'}
-            </span>
-          )}
-        </div>
-      </div>
-      <div className="trippage_chat-section">
-        <div className="trippage_profile-image">
-          <div className="trippage_profile-picture">
-            <img src="" alt="" />
-          </div>
-          <div className="trippage_profileName">User</div>
-        </div>
-        <div className="grey-line2"></div>
-        <div className="trippage_date-container">
-          <div className="trippage_date-section">
-            <div className="trippage_startdate trippage_details">
-              <div className="trippage_box_heading">Start Date</div>
-              <div className="trippage_box_content">DD/MM/YYYY</div>
-            </div>
-            <div className="trippage_enddate trippage_details">
-              <div className="trippage_box_heading">End Date</div>
-              <div className="trippage_box_content">DD/MM/YYYY</div>
-            </div>
-          </div>
-          <div className="trippage_info-section">
-            <div className="trippage_budget trippage_details">
-              <div className="trippage_box_heading">Budget</div>
-              <div className="trippage_box_content">6000</div>
-            </div>
-            <div className="trippage_totalMember trippage_details">
-              <div className="trippage_box_heading">Members</div>
-              <div className="trippage_box_content">10</div>
-            </div>
-          </div>
-          <div className="trippage_chat-now-button">Chat Now</div>
-        </div>
-      </div>
-    </div>
+          {words.length > 40 && <ToggleButton onClick={toggleExpand}>{isExpanded ? ' Show Less' : ' Show More'}</ToggleButton>}
+        </DescriptionContent>
+      </DescriptionContainer>
+      <ChatSection>
+        <ProfileImage>
+          <ProfilePicture src="" alt="" />
+          <ProfileName>User</ProfileName>
+        </ProfileImage>
+        <GreyLine />
+        <DateContainer>
+          <DateSection>
+            <StartDate>
+              <BoxHeading>Start Date</BoxHeading>
+              <BoxContent>DD/MM/YYYY</BoxContent>
+            </StartDate>
+            <EndDate>
+              <BoxHeading>End Date</BoxHeading>
+              <BoxContent>DD/MM/YYYY</BoxContent>
+            </EndDate>
+          </DateSection>
+          <InfoSection>
+            <DetailsBox>
+              <BoxHeading>Budget</BoxHeading>
+              <BoxContent>6000</BoxContent>
+            </DetailsBox>
+            <DetailsBox>
+              <BoxHeading>Members</BoxHeading>
+              <BoxContent>10</BoxContent>
+            </DetailsBox>
+          </InfoSection>
+          <ChatButton>Chat Now</ChatButton>
+        </DateContainer>
+      </ChatSection>
+    </SectionContainer>
   )
 }
 
