0a5460ca179d81b795811afd671aae9cbab360bc
akshat170902 <akshat.ug20@nsut.ac.in>
2024-11-19 21:43:05 +0530

    Chat box

--- src/services/api-services/config/api-constants.js
+++ src/services/api-services/config/api-constants.js
@@ -37,8 +37,6 @@ export const API_PATH = {
 export const API_CONFIG = {
   headers: {
     'Authorization': '',
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
     'GoogleAuthorization': '',
   },
 }
