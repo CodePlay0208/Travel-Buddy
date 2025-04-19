be6d630d3643e4c256073f13f516a55c5e4ab0ba
akshat170902 <akshat.ug20@nsut.ac.in>
2025-04-19 23:40:56 +0530

    Add webpack configuration for production build setup

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js', // Adjust if your entry point is different
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'static/js/[contenthash:8].js',
+    chunkFilename: 'static/js/[contenthash:8].chunk.js',
+    publicPath: '/',
+    clean: true, // Cleans the output directory before emit
+  },
+  devtool: false, // Disables source maps in production
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'static/media/[name].[contenthash:8][ext]',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      name: false,
+    },
+    runtimeChunk: {
+      name: (entrypoint) => `runtime-${entrypoint.name}`,
+    },
+  },
+}
