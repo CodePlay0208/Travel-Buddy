12bc0a9018534694a2b3807cdd0a5b94a0f2f203
akshat170902 <akshat.ug20@nsut.ac.in>
2025-05-31 19:35:39 +0530

    fix: Correct day title and description mapping in ItineraryPreview component

--- src/screens/PublishTrip/ItineraryPreview.js
+++ src/screens/PublishTrip/ItineraryPreview.js
@@ -23,10 +23,10 @@ const ItineraryPreview = ({ tripData, margin }) => {
 
   return (
     <Container ref={containerRef} $height={height} margin={margin}>
-      <DayTitle>{tripData?.inc_excTitle}</DayTitle>
+      <DayTitle>{tripData?.dayTitle}</DayTitle>
       <Content>
         <List>
-          {tripData?.inc_excDescription?.map((text, idx) => (
+          {tripData?.dayDescription?.map((text, idx) => (
             <ListItem key={idx}>{text}</ListItem>
           ))}
         </List>
